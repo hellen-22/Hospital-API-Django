4eb0c6fce1460fc985d9a294c214a3dfe822ab89
Hellen Wainaina <hellenwain@gmail.com>
2022-11-10 09:43:47 +0300

    Creating models

--- custom/models.py
+++ custom/models.py
@@ -4,3 +4,10 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'username', 'phone_number']
+
+    def __str__(self) -> str:
+        return self.username
---
+++ details/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+
+
+from custom.models import User
+# Create your models here.
+
+class Speciality(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self) -> str:
+        return self.name
+
+class Doctor(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=100)
+    speciality = models.ForeignKey(Speciality, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.user.username
+
+class Nurse(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.user.username
+
+class Pharmacy(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.user.username
+
+
+class Lab(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.user.username
+
+class DoctorAvailability(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    time = models.TimeField()
+    date = models.DateField()
+
+    def __str__(self) -> str:
+        return self.doctor.user.username
